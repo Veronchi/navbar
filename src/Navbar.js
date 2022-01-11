4b4df897c3b918b1ac618ab059674edfe3c75c07
Veronchi <zhigunova1995@gmail.com>
2022-01-11 18:39:16 +0300

    implement the layout of the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,38 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='logo' alt='logo'/>
+          <button className='nav-toggle'></button>
+        </div>
+        <div className='links-container'>
+          <ul className='links'>
+            {links.map(({id, url, text}) => {
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {social.map(({ id, url, icon }) => {
+            return (
+              <li key={id}>
+                <a href={url}>
+                  {icon}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
